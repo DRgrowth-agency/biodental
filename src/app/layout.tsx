12ff88197a5311d5a889ff0a3eae37558a46a91b
fodilben <fben222866@gmail.com>
2025-12-30 21:25:55 +0100

    seo :add the scritp in the layotu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,9 +11,41 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-     
-      >
+      <head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Dentist",
+              "name": "BioDental",
+              "url": "https://biodental-dr-fetnaci.com",
+              "telephone": "+213787907832",
+              "email": "biodental.dr.fetnaci@gmail.com",
+              "address": {
+                "@type": "PostalAddress",
+                "streetAddress": "8 mars, Plaine Ouest, cité 1172 lgts, bloc 60",
+                "addressLocality": "Annaba",
+                "addressCountry": "DZ"
+              },
+              "openingHoursSpecification": [
+                {
+                  "@type": "OpeningHoursSpecification",
+                  "dayOfWeek": ["Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday"],
+                  "opens": "09:00",
+                  "closes": "18:00"
+                }
+              ],
+              "sameAs": [
+                "https://web.facebook.com/Biodental.Dr.Fetnaci.S/",
+                "https://www.instagram.com/dr.fetnaci_sofiane",
+                "https://www.tiktok.com/@biodental_dr.fetnaci"
+              ]
+            })
+          }}
+        />
+      </head>
+      <body>
         {children}
       </body>
     </html>
